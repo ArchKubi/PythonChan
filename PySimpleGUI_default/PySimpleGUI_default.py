9a11154b7b149d6b6f42b96f13e1e0273a94c9a0
archkubi <jayjameshlw@gmail.com>
2022-06-01 21:20:29 +0300

    ywy

--- PySimpleGUI_default/PySimpleGUI_default.py
+++ PySimpleGUI_default/PySimpleGUI_default.py
@@ -1,71 +1,24 @@
-# -*- coding:utf-8 -*-
-from urllib import request
-import PySimpleGUI as sg
-import requests
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+from random import randint
 
-def border(elem):
-    return sg.Frame('', [[elem]], background_color='#0b011c')
+app = Ursina()
+player = FirstPersonController(collider="box")
+ground = Entity(model="plane",collider="mesh",texture="grass",scale=(30,0,3))
 
-sg.set_options(font="Sans 20")
-sg.LOOK_AND_FEEL_TABLE['MyCreatedTheme'] = {
-                                        'BACKGROUND': '#240046',
-                                        'TEXT': '#9d4edd',
-                                        'INPUT': '#339966',
-                                        'TEXT_INPUT': '#9d4edd',
-                                        'SCROLL': '#5a189a',
-                                        'BUTTON': ('#c77dff',  '#3c096c'),
-                                        'PROGRESS': ('#c77dff', '#3c096c'),
-                                        'BORDER': 1, 'SLIDER_DEPTH': 0,
-                                        'PROGRESS_DEPTH': 0, 
-                                            }
+p1 = Entity(model="plane",color=color.violet,scale=(0.4,0.1,53),z=28,x=0.7)
+p2 = duplicate(p1, x=-3.7)
+p3 = duplicate(p1, x=-0.6)
+p4 = duplicate(p1, x=3.6)
 
+blocks = []
+for f in range(12):
+	block = Entity(model="cube",color=color.white33,
+position=(2,0.1,3+f*4),scale=(3,0.1,2.5))
 
-url = "https://i.ibb.co/jZzSq6Q/logo.png"
-response = requests.get(url, stream=True)
-response.raw.decode_content = True
+block2 = duplicate(block, x=-2.2)
+blocks.append( 
+(block,block2,randint(0,3)>0, randint(0,3)>0) 
+)
 
-sg.theme("MyCreatedTheme")
-
-layout = [
-        [sg.Text("Welcome to GnuChan Calculator", expand_x=True,justification="center")],
-        [border(sg.Image(size=(500,500), data=response.raw.read(), expand_x=True, expand_y=True, background_color="#19032e"))],
-        
-        
-        ]
-
-tab_group = [
-    [
-            sg.TabGroup
-        (
-            [[
-            sg.Tab("Default", layout),
-            sg.Button("Exit", expand_x=True),
-            ]],
-
-            tab_location="center",
-            title_color="#9d4edd",
-            tab_background_color="#370666",
-            selected_title_color="#c77dff",
-            selected_background_color="#240046",
-            key='Status'
-        ),
-    ]
-]
-
-layout = [[sg.Text('GnuChan', size=(30, 1), key='Status')]]
-window = sg.Window('Title', layout, resizable=True, finalize=True)
-window.bind('<Configure>', "Configure")
-status = window['Status']
-
-while True:
-
-    event, values = window.read()
-    if event == sg.WINDOW_CLOSED:
-        break
-    elif event == 'Configure':
-        if window.TKroot.state() == 'zoomed':
-            status.update(value='Window zoomed and maximized !')
-        else:
-            status.update(value='Window normal')
-
-
+app.run()
